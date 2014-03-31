ffdf7db83aae0e0f32773715274c2de7be4b1287
lli <lli@localhost.localdomain>
2014-03-31 16:39:18 -0400

    setting

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+BASE_DIR = os.path.realpath(os.path.dirname(__file__)) + '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -27,8 +26,7 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
+# -------------------------Application---------------------
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,6 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TT',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -45,38 +44,67 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.transaction.TransactionMiddleware',
 )
 
+#--------------login----------------------
+LOGIN_URL = '/TT/login'
+# Default URL to redirect to after a user logs in.
+LOGIN_REDIRECT_URL = '/TT/'
+
+#-----------------email--------------------
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST='smtp.andrew.cmu.edu'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=True
+
+
+#-------------------------------------------
 ROOT_URLCONF = 'final_project.urls'
 
 WSGI_APPLICATION = 'final_project.wsgi.application'
 
 
-# Database
+#-------------------Database-------------------
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR+'db/TT.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '', 
     }
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = 'static/'
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+#------------media files------------
+MEDIA_ROOT = BASE_DIR + 'media/'
+MEDIA_URL = '/media/'
+
+
+#-------------template-------------
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
