619bc10ad7e971d213646a8cf6150c5508717d05
lli <lli@localhost.localdomain>
2014-03-31 20:45:06 -0400

    d

--- final_project/TT/models.py
+++ final_project/TT/models.py
@@ -2,86 +2,82 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User
 
-class Buyer(models.Model):
-	user = models.ForeignKey(User)
-	orders = models.ManyToManyField(Order)
 
 
-	def _unicode_(self):
-		return 'Buyer:'+self.user.email
-
-class Seller(models.Model):
-	user = models.ForeignKey(User)
-	orders = models.ManyToManyField(Order)
+# class Buyer(models.Model):
+# 	user = models.ForeignKey(User)
+# 	orders = models.ManyToManyField(Order)
 
 
-	def _unicode_(self):
-		return 'Seller:'+self.user.email
+# 	def _unicode_(self):
+# 		return 'Buyer:'+self.user.email
 
-class Wallet(models.Model):
-	user = models.ForeignKey(User)
-	balance = models.IntegerField()
+# class Seller(models.Model):
+# 	user = models.ForeignKey(User)
+# 	orders = models.ManyToManyField(Order)
 
-	def _unicode_(self):
-		return 'Wallet:'+self.user.email
 
-class Item(models.Model):
-	user = models.ForeignKey(User)
-	name = models.CharField(max_length = 200)
-	tag = models.IntegerField()
-	description = models.CharField(max_length = 2000)
-	images = ManyToManyField(ImageDesc)
+# 	def _unicode_(self):
+# 		return 'Seller:'+self.user.email
 
-	def _unicode_(self):
-		return 
+# class Wallet(models.Model):
+# 	user = models.ForeignKey(User)
+# 	balance = models.IntegerField()
 
-class Order(models.Model):
-	buyer = models.ForeignKey(Buyer)
-	seller = models.ForeignKey(Seller)
-	items = models.ManyToManyField(Item)
+# 	def _unicode_(self):
+# 		return 'Wallet:'+self.user.email
 
-	def _unicode_(self):
-		return ""
+# class Item(models.Model):
+# 	user = models.ForeignKey(User)
+# 	name = models.CharField(max_length = 200)
+# 	tag = models.IntegerField()
+# 	description = models.CharField(max_length = 2000)
+# 	images = ManyToManyField(ImageDesc)
 
-class Document(models.Model):
-	user = models.ForeignKey(User)
+# 	def _unicode_(self):
+# 		return 
 
 
 
-	def addDocumentValue():
-
-		return
-
-	def removeDocumentValue():
-
-		return
-
-	def updateDocumentValue():
-
-		return
-
-	def _unicode_(self):
-		return 'Document:'+self.user.email
-
-class DocumentValue(models.Model):
-	doc = models.ForeignKey(Document)
-	tag = models.IntegerField()
-	orderNum = models.IntegerField()
-	totalPrice = models.IntegerField()
-
-	def _unicode_(self):
-		return self.orderNum+" :"+self.totalPrice
-
-class ImageDesc(models.Model):
-	pic = models.ImageField(upload_to="TT-pics", blank=True)
-	description = models.CharField(max_length=200)
-
-	def _unicode_(self):
-		return self.description
+# class Document(models.Model):
+# 	user = models.ForeignKey(User)
 
 
-class UserToken(models.Model):
-	user = models.ForeignKey(User)
-	token = models.CharField(max_length=200)
-	def __unicode__(self):
-		return self.token 
+
+# 	def addDocumentValue():
+
+# 		return
+
+# 	def removeDocumentValue():
+
+# 		return
+
+# 	def updateDocumentValue():
+
+# 		return
+
+# 	def _unicode_(self):
+# 		return 'Document:'+self.user.email
+
+# class DocumentValue(models.Model):
+# 	doc = models.ForeignKey(Document)
+# 	tag = models.IntegerField()
+# 	orderNum = models.IntegerField()
+# 	totalPrice = models.IntegerField()
+
+# 	def _unicode_(self):
+# 		return self.orderNum+" :"+self.totalPrice
+
+# class ImageDesc(models.Model):
+# 	pic = models.ImageField(upload_to="TT-pics", blank=True)
+# 	description = models.CharField(max_length=200)
+
+# 	def _unicode_(self):
+# 		return self.description
+
+
+# class UserToken(models.Model):
+# 	user = models.ForeignKey(User)
+# 	token = models.CharField(max_length=200)
+# 	def __unicode__(self):
+# 		return self.token 
